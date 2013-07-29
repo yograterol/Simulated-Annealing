c4163277d672a18e825844e8cc16d3dda85c6652
Yohan Graterol <yohangraterol92@gmail.com>
2013-07-28 23:33:43 -0430

    Fixed the election of the neighbor city

--- recocido_simulado.py
+++ recocido_simulado.py
@@ -8,6 +8,8 @@ from collections import deque
 from math import exp
 from numpy.random import (permutation, random_sample)
 from numpy import log
+from copy import deepcopy
+from random import randint
 
 
 class LoadData(object):
@@ -42,7 +44,7 @@ class SimulatedAnnealing(object):
     __slots__ = ['matrix', 'T', 't', 't_final', 'step', 'cities', 'firts_vc',
                  'Vc', 'Vn', 'Vc_eval', 'Vn_eval', 'alpha', 'solutions']
 
-    def __init__(self, T=1000, alpha=0.90, t_final=1, t=1, cities=29, step=200):
+    def __init__(self, T=1000, alpha=0.9899, t_final=0.001, t=1, cities=29, step=550):
         data = LoadData()
         data.create_matrix()
         self.matrix = data.matrix
@@ -60,20 +62,20 @@ class SimulatedAnnealing(object):
 
     def tsp(self):
         self.Vc = self.generate_solution()
-        self.Vc_eval = self.eval_solution(self.Vc)
+        #self.Vc_eval = self.eval_solution(self.Vc)
 
         while(self.T > self.t_final):
             for i in range(self.step):
                 self.Vn = self.generate_solution(self.Vc)
-                self.Vn_eval = self.eval_solution(self.Vn)
-                if self.Vn_eval - self.Vc_eval < 0:
+                #self.Vn_eval = self.eval_solution(self.Vn)
+                delta = self.eval_solution(self.Vn) - self.eval_solution(self.Vc)
+                if delta < 0:
                     self.Vc = self.Vn
-                    self.Vc_eval = self.Vn_eval
-                elif random_sample() < exp(-(self.Vn_eval - self.Vc_eval)/self.T):
+                elif random_sample() < exp(-delta/self.T):
                     self.Vc = self.Vn
-                    self.Vc_eval = self.Vn_eval
-            self.T *= self.reduce_temp(self.t)
-            self.t += 1
+            self.T *= self.alpha
+	        #self.T *= self.reduce_temp(self.t)
+            #self.t += 1
 
     def reduce_temp(self, t):
         return self.alpha / log(1 + t)
@@ -81,14 +83,13 @@ class SimulatedAnnealing(object):
     def generate_solution(self, Vc=None):
         if Vc is None:
             Vn = list(permutation(self.firts_vc))
-            self.solutions.append(Vn)
             return Vn
         if Vc:
-            Vn = list(permutation(Vc))
-            while(Vn in self.solutions):
-                Vn = permutation(Vc)
-            self.solutions.append(Vn)
-            return Vn
+            Vn = deepcopy(Vc)
+	    i1 = randint(0, self.cities - 1)
+	    i2 = randint(0, self.cities - 1)
+	    Vn[i1], Vn[i2] = Vn[i2], Vn[i1]
+        return Vn
 
     def eval_solution(self, Vn, debug=False):
         km = 0
@@ -96,19 +97,21 @@ class SimulatedAnnealing(object):
         for i in enumerate(Vn):
             if not firts_city:
                 firts_city = i
-            i = int(i[0])
-            j = int(i) + 1
-            if j == self.cities:
-                i = firts_city[0]
-                j -= 1
 
-            i = Vn[i]
+            i1 = i[0]
+            j = i1 + 1
+            if j == self.cities:
+                i = firts_city[1]
+                j -= 1
+	    else:
+                i = i[1]
+
             j = Vn[j]
 
             if i > j:
                 i, j = j, i
             if debug:
-                print i, j
+                print i, j, self.matrix[i][j]
             km += int(self.matrix[i][j])
         return km
 
